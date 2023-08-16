aa7190699a9b960633d9ec8bee82421955b27ae7
Sampow360 <134953678+Sampow360@users.noreply.github.com>
2023-08-15 19:01:13 -0500

    2 files modified

--- index.ts
+++ index.ts
@@ -65,7 +65,7 @@ this.load.spritesheet('S_IdleR', 'static/assets/S_Sprites/S_idleR.png', {
 this.load.animation('S_Anims', './static/assets/S_Anims.json');
 
 // Preload the tileset image
-this.load.image('EnviromentalTiles1', 'static/assets/Tiled/EnviromentalTiles1.png');
+this.load.image('EnviromentalTiles1', 'static/assets/Tiled/EnviromentalTiles1v2.png');
 
 // Preload the tilemap in Tiled JSON format
 this.load.tilemapTiledJSON('test', 'static/assets/Tiled/test.json');
